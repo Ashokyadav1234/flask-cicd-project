67ca5bd9c0a233b9d98baf30a66d1764a7865b11
Ashok Yadav <ashoky1610@gmail.com>
2026-09-20 23:46:08 +0530

    Update Python course price

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ courses = [
     {
         "name": "Python Mastery",
         "category": "Programming",
-        "price": 799,
+        "price": 99,
         "description": "Learn Python from basics to advanced concepts."
     },
     {
